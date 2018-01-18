61b2753e0db485d8c6db340e309104e32f250590
robinhood14 <35225066+robinhood14@users.noreply.github.com>
2018-01-18 22:50:53 +0000

    Update crypto.rb

--- crypto.rb
+++ crypto.rb
@@ -6,32 +6,25 @@ require 'open-uri'
 page = Nokogiri::HTML(open("https://coinmarketcap.com/all/views/all/")) #on va diriger Nokogiri vers la page coin marketcap
 
 name_list = page.css('a.currency-name-container') #On repere le selecteur du nom des coins et on stock dans l'objet page.css qu'on nomme name_list , on va faire appel à ça par la suite en utlisant le .text qui va chercher le texte de la balise , dans notre cas le nom du coin
-price_list = page.css('a.price') #On va de même stocker la liste de prix, on rpère son selecteur : a.price, et la stocke avec la methode Nokogiri dans price_list, on va par la suite appeler ['data-usd'] pour donner le prix du coin
-
+price_list = page.css('a.price') #On stock la liste de prix, on repère son selecteur : a.price, et la stocke avec la methode Nokogiri dans price_list, on va par la suite appeler ['data-usd'] pour donner le prix du coin
 tab = Hash.new #on crée un nouveau Hash, dans lequel on va stocker le nom des coin et le prix associé
 
-
 =begin
-    Elements sur le code dessous :
-    on va faire prendre à notre hash à chaque fois l'entrée : nom coin ==> prix coin
-    on va donc utiliser .text et ['data-usd'] pour chaque éléments de nos array définits dessus
-    pour y extraire le nom du coin et le prix associé
-    Ici nous avons la chance que la longeur des 2 tableau (prix, coin)
-sont identiques
-    on crée donc une boucle for qui commence à 0 et se termine à name_list.length-1 : qui
-    correspond à la longueur de notre tableau
-    et on termine par puts notre hash
+
+   
+  
+    
 =end
 
-for i in 0 .. (name_list.length-1)
-    tab[name_list[i].text] = [price_list[i]['data-usd']]
+for i in 0 .. (name_list.length-1)#on crée donc une boucle for qui commence à 0 et se termine à name_list.length-1 : qui correspond à la longueur de notre tableau
+    tab[name_list[i].text] = [price_list[i]['data-usd']]# on fait prendre à notre hash à chaque fois l'entrée : nom coin et prix coin en utilisant les méthodes .text et ['data-usd'] pour chaque éléments de nos array définits (L8-10)
 end
 
+puts tab #on termine par puts notre hash
 
-puts tab
-
-=begin ce code ne fonctionne pas 
+=begin
+ce code ne fonctionne pas 
 puts "   "
 puts "sleeping 5 sec"
-sleep(3600)   # on rajoute une methode qui permet de relancer le programme toute les heures (3600)
+sleep(3600)   # on rajoute une methode sleep qui permet de relancer le programme toute les heures (3600)
 =end
