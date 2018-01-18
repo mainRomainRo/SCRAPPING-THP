d7704f858562b2bb5356111aa1ac60ba63b76a04
robinhood14 <35225066+robinhood14@users.noreply.github.com>
2018-01-18 22:57:38 +0000

    Update crypto.rb

--- crypto.rb
+++ crypto.rb
@@ -2,25 +2,21 @@ require 'rubygems'
 require 'nokogiri' 
 require 'open-uri'  
 
+=begin
+Creation d'un programme qui va récupérer le cours de toutes les cryptomonnaies sur le site 
+https://coinmarketcap.com/all/views/all/. Ce programme tourne en boucle, et affiche les cours toutes les heures.
+=end
 
 page = Nokogiri::HTML(open("https://coinmarketcap.com/all/views/all/")) #on va diriger Nokogiri vers la page coin marketcap
-
-name_list = page.css('a.currency-name-container') #On repere le selecteur du nom des coins et on stock dans l'objet page.css qu'on nomme name_list , on va faire appel à ça par la suite en utlisant le .text qui va chercher le texte de la balise , dans notre cas le nom du coin
+name_list = page.css('a.currency-name-container') #On repere le selecteur du nom des coins et on stock dans l'objet page.css qu'on nomme name_list , on va faire appel à ça par la suite en utilisant la méthode .text qui va chercher le texte de la balise , dans notre cas le "nom du coin"
 price_list = page.css('a.price') #On stock la liste de prix, on repère son selecteur : a.price, et la stocke avec la methode Nokogiri dans price_list, on va par la suite appeler ['data-usd'] pour donner le prix du coin
 tab = Hash.new #on crée un nouveau Hash, dans lequel on va stocker le nom des coin et le prix associé
 
-=begin
-
-   
-  
-    
-=end
-
 for i in 0 .. (name_list.length-1)#on crée donc une boucle for qui commence à 0 et se termine à name_list.length-1 : qui correspond à la longueur de notre tableau
-    tab[name_list[i].text] = [price_list[i]['data-usd']]# on fait prendre à notre hash à chaque fois l'entrée : nom coin et prix coin en utilisant les méthodes .text et ['data-usd'] pour chaque éléments de nos array définits (L8-10)
+    tab[name_list[i].text] = [price_list[i]['data-usd']]# on fait prendre à notre hash à chaque fois l'entrée : "nom du coin" et "prix du coin" en utilisant les méthodes .text et ['data-usd'] pour chaque éléments de nos array définits (L8-10)
 end
 
-puts tab #on termine par puts notre hash
+puts tab #on termine par afficher notre hash sur le terminal
 
 =begin
 ce code ne fonctionne pas 
